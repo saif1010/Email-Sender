87e7c54be78f66b4b921abd6a50957f97da61de7
saif1010 <76566339+saif1010@users.noreply.github.com>
2023-09-25 21:35:47 +0530

    Add files via upload

---
+++ Email-Sender-main/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import EmailSender from './Component/EmailSender/EmailSender'
+function App() {
+
+
+  return (
+    <>
+   
+      <EmailSender/>
+         
+    </>
+  )
+}
+
+export default App
