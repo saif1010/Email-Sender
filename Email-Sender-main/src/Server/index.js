b3c8ba2012b036a642954be863296f97c1a96021
saif1010 <76566339+saif1010@users.noreply.github.com>
2023-09-25 21:37:17 +0530

    Update index.js

--- Email-Sender-main/src/Server/index.js
+++ Email-Sender-main/src/Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const app = express();
-const port = 3000;
+const port = 8080;
 require('dotenv').config();
 
 app.use(express.json());
@@ -31,11 +31,11 @@ app.post('/send-email', async (req, res) => {
   // Define the email options
   const mailOptions = {
     from:{
-        name : 'Anmol Jain',
+        name : 'Saiful Islam',
         address : process.env.USER
       },
     to: recipient,
-    subject: 'Hello from Node.js',
+    subject: 'Hello from Saiful',
     html: content,
   };
 
